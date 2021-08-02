d0743b3173bea2f54295900168f081ce1cec5b8e
Gokhulnath <gokhulnath18051@cse.ssn.edu.in>
2021-08-03 00:02:06 +0530

    ex2 done

---
+++ EX2/code/RL_-_-1.cpp
@@ -0,0 +1,66 @@
+#include<GL/glut.h>
+#include<stdlib.h>
+#include<stdio.h>
+#include<cstdlib>
+
+void display(void)
+{
+	float dy, dx, step, x, y, k, Xin, Yin;
+	float x1 = 0;
+	float y1 = 0;
+	float x2 = -60;
+	float y2 = 20;
+	dx = x2 - x1;
+	dy = y2 - y1;
+
+	if (abs(dx) > abs(dy))
+	{
+		step = abs(dx);
+	}
+	else
+		step = abs(dy);
+
+	Xin = dx / step;
+	Yin = dy / step;
+
+	x = x1;
+	y = y1;
+	glBegin(GL_POINTS);
+	glVertex2i(x, y);
+	glEnd();
+
+	for (k = 1; k <= step; k++)
+	{
+		x = x + Xin;
+		y = y + Yin;
+
+		glBegin(GL_POINTS);
+		glVertex2i(x, y);
+		glEnd();
+	}
+
+
+	glFlush();
+}
+
+void init(void)
+{
+	glClearColor(0.7, 0.7, 0.7, 0.7);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluOrtho2D(-100, 100, -100, 100);
+}
+
+int main(int argc, char** argv) {
+
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+	glutInitWindowSize(500, 500);
+	glutInitWindowPosition(100, 100);
+	glutCreateWindow("EX2 DDA");
+	init();
+	glutDisplayFunc(display);
+	glutMainLoop();
+
+	return 0;
+}
